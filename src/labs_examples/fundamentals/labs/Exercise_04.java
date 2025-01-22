a8815d5d1b27dae6510bfc40583bd0057af85612
Bradford Duncan <BradfordTDuncan@gmail.com>
2025-01-21 22:09:20 -0600

    Fundamentals labs completed

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -12,10 +12,41 @@ public class Exercise_04 {
 
     public static void main(String[] args) {
 
-        int i = 1;
+
+        int i = 999999999;
         System.out.println("int i is: " + i);
 
         // write your code below
+        boolean boolAnswer = false;
+        System.out.println("the variable boolAnser is: " + boolAnswer);
+
+        char charActer = 'b';
+        System.out.println("the variable charActer contains: " + charActer);
+
+        String stringOfChars = "String is string";
+        System.out.println("the variable stringOfChars contains: " + stringOfChars);
+
+        byte byteNum = 123;
+        System.out.println("the variable byteNum contains: " + byteNum);
+
+        short shortNum = 12345;
+        System.out.println(("shortNum: " + shortNum));
+
+        long longNum = 1234567890;
+        System.out.println("longNum: " + longNum);
+
+        float floatNum = 1234567890.00000001f;
+        System.out.println("floatNum: " + floatNum);
+
+        double doubleNum = 12345.123456789123456789123456789d;
+        System.out.println("doubleNum: " + doubleNum);
+
+        i++;
+        System.out.println("int i is: " + i);
+
+
+
+
 
     }
 
--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -8,20 +8,27 @@ package labs_examples.fundamentals.labs;
  *
  */
 
-public class Exercise_05 {
+public class  Exercise_05 {
 
     public static void main(String[] args) {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
+        int strLength = str.length();
+
+        System.out.println(strLength);
 
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
+        boolean stringTest = str == str2;
 
         // please concatenate str & str2 and set the result to a new String variable below
+        String str3 = str + str2;
+        System.out.println(str3);
 
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
+        System.out.println(str3.indexOf('l'));
 
     }
 
